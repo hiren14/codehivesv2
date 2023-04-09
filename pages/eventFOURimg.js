4ab6fd88470e336d9a141b62bb8c644b1d4333b1
yashvi78 <yashviviradiya603@gmail.com>
2023-04-09 10:57:00 +0530

    Add eventFourimg.js and  add Event4 images

---
+++ pages/eventFOURimg.js
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+import EventCard from '../components/event/EventCard';
+import { eventFOURimg } from '../lib/data/EventimgData';
+import Screen from '../components/screen/Screen';
+import BottomGlitter from '../components/StyledText/BottomGlitter';
+import Styles from '../components/event/event.module.css';
+
+function EVEntimg() {
+  return (
+    <Screen>
+      <Head>
+        <title>About Us</title>
+        <meta
+          name="description"
+          content="HnCC or Hackathon and Coding Club is the official club of BIT Sindri that has the motto to instill a coding culture, collaborate, and arrange events relevant to Open Source, Graphics and Game Development, Web Development, App Development, and many other topics."
+        />
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript, ReactJS, NextJS, TailwindCSS, HnCC, About HnCC, Hackathon & Coding Club, BIT Sindri, Dhanbad"
+        />
+      </Head>
+      
+      <section className="container-70 pt-28 md:pt-16 sm:pt-12">
+      <BottomGlitter text="Our Events" />
+      <div className={Styles.cardContainer}>
+        {eventFOURimg.map((item, index) => {
+          return (
+            <EventCard
+              title={item.title}
+              description={item.description}
+              link={item.link}
+              image={item.src}
+              key={`${index + item.title}`}
+            />
+          );
+        })}
+      </div>
+    
+      </section>
+    </Screen>
+  );
+}
+
+export default EVEntimg;
